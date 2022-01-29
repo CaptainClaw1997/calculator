b80a0af857b3ecaf41430b43f8c6d72654c4b8bc
CaptainClaw1997 <bhari.elan@gmail.com>
2022-01-29 12:33:32 +0530

    Add event listener and restructure files

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-import * as Calculator from "./calculator.js";
+const add = function(num1, num2) {
+	return (num1 + num2);
+};
+
+const subtract = function(num1, num2) {
+	return (num1 - num2);
+};
+
+const sum = function(numArray) {
+	let sum = 0;
+  numArray.forEach((num) => sum += num);
+
+  return sum;
+};
+
+const multiply = function(numArray) {
+  let product = 1;
+  numArray.forEach((num) => product *= num);
+
+  return product;
+};
+
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
+};
+
+const factorial = function(num) {
+  let factorial = 1;
+	if (num === 0 || num === 1) return factorial;
+  else {
+    for(let i = num; i > 0; i --) {
+      factorial *= i;
+    }
+  }
+
+  return factorial;
+};
 
 function operate(num1, num2, operator) {
     switch (operator) {
@@ -14,4 +50,14 @@ function operate(num1, num2, operator) {
         case '/':
             return Calculator.divide(num1, num2);
     }
+}
+
+const buttons = [...document.querySelectorAll(buttons)];
+
+function addEventListenersToButtons(buttons) {
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            console.log(e.target);
+        })
+    });
 }
